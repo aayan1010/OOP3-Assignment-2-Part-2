7f8d1c259611b545640138cda3c0e29e934f1f2f
Aayan Karim <40751399+aayan1010@users.noreply.github.com>
2026-04-02 18:28:29 -0600

    Add Stack/Queue ADTs and MyArrayList

---
+++ assignment2StartingCode/src/utilities/MyArrayList.java
@@ -0,0 +1,149 @@
+package utilities;
+
+public class MyArrayList<E> implements ListADT<E> {
+    private E[] elements;
+    private int size;
+    private static final int DEFAULT_CAPACITY = 10;
+
+    // Constructor
+    @SuppressWarnings("unchecked")
+    public MyArrayList() {
+        elements = (E[]) new Object[DEFAULT_CAPACITY];
+        size = 0;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public void clear() {
+        for (int i = 0; i < size; i++) {
+            elements[i] = null;
+        }
+        size = 0;
+    }
+
+    @Override
+    public boolean add(int index, E toAdd) throws NullPointerException, IndexOutOfBoundsException {
+        if (toAdd == null) throw new NullPointerException("Cannot add null element");
+        if (index < 0 || index > size) throw new IndexOutOfBoundsException("Invalid index");
+
+        ensureCapacity();
+
+        for (int i = size; i > index; i--) {
+            elements[i] = elements[i - 1];
+        }
+        elements[index] = toAdd;
+        size++;
+        return true;
+    }
+
+    @Override
+    public boolean add(E toAdd) throws NullPointerException {
+        return add(size, toAdd); // Add to the very end
+    }
+
+    @Override
+    public boolean addAll(ListADT<? extends E> c) throws NullPointerException {
+        if (c == null) throw new NullPointerException();
+        Iterator<? extends E> iterator = c.iterator();
+        while (iterator.hasNext()) {
+            add(iterator.next());
+        }
+        return true;
+    }
+
+    @Override
+    public E get(int index) throws IndexOutOfBoundsException {
+        if (index < 0 || index >= size) throw new IndexOutOfBoundsException("Invalid index");
+        return elements[index];
+    }
+
+    @Override
+    public E remove(int index) throws IndexOutOfBoundsException {
+        if (index < 0 || index >= size) throw new IndexOutOfBoundsException("Invalid index");
+        E removedElement = elements[index];
+        
+        for (int i = index; i < size - 1; i++) {
+            elements[i] = elements[i + 1];
+        }
+        elements[size - 1] = null;
+        size--;
+        return removedElement;
+    }
+
+    @Override
+    public E remove(E toRemove) throws NullPointerException {
+        if (toRemove == null) throw new NullPointerException();
+        for (int i = 0; i < size; i++) {
+            if (elements[i].equals(toRemove)) {
+                return remove(i);
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public E set(int index, E toChange) throws NullPointerException, IndexOutOfBoundsException {
+        if (toChange == null) throw new NullPointerException();
+        if (index < 0 || index >= size) throw new IndexOutOfBoundsException();
+        
+        E oldElement = elements[index];
+        elements[index] = toChange;
+        return oldElement;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    @Override
+    public boolean contains(E toFind) throws NullPointerException {
+        if (toFind == null) throw new NullPointerException();
+        for (int i = 0; i < size; i++) {
+            if (elements[i].equals(toFind)) return true;
+        }
+        return false;
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public E[] toArray(E[] toHold) throws NullPointerException {
+        if (toHold == null) throw new NullPointerException();
+        if (toHold.length < size) {
+            toHold = (E[]) java.lang.reflect.Array.newInstance(toHold.getClass().getComponentType(), size);
+        }
+        for (int i = 0; i < size; i++) {
+            toHold[i] = elements[i];
+        }
+        return toHold;
+    }
+
+    @Override
+    public Object[] toArray() {
+        Object[] newArray = new Object[size];
+        for (int i = 0; i < size; i++) {
+            newArray[i] = elements[i];
+        }
+        return newArray;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return new MyIterator<>(this);
+    }
+
+    @SuppressWarnings("unchecked")
+    private void ensureCapacity() {
+        if (size == elements.length) {
+            E[] newElements = (E[]) new Object[elements.length * 2];
+            for (int i = 0; i < size; i++) {
+                newElements[i] = elements[i];
+            }
+            elements = newElements;
+        }
+    }
+}
